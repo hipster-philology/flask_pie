56bd6e66490146fff2ac86b06d7bfcc99cd66e65
Thibault Clérice <leponteineptique@gmail.com>
2019-12-17 14:20:21 +0100

    (0.0.7) Upgraded Pie dependancy

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ DESCRIPTION = "Web API for NLP tagger PIE"
 URL = 'https://github.com/ponteineptique/flask_pie'
 AUTHOR = 'Thibault Clérice'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.0.6"
+VERSION = "0.0.7"
 
 # What packages are required for this module to be executed?
 
